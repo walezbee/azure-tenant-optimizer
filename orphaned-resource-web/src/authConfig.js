5da4bbd30bc0b4fb49987cfb4cf56fdcc1c8ac66
Wale <walezbee@gmail.com>
2025-07-21 12:01:45 +0200

    Access update to authconfig.js file

--- orphaned-resource-web/src/authConfig.js
+++ orphaned-resource-web/src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "2bed0b07-e8ff-4333-9c8b-2aa020872e2d",
     authority: "https://login.microsoftonline.com/common", // Enables multi-tenant access
-    redirectUri: "https://ambitious-moss-0f4949803.1.azurestaticapps.net"
+    redirectUri: "https://ambitious-moss-0f4949803.1.azurestaticapps.net/.auth/login/aad/callback"
   },
   cache: {
     cacheLocation: "sessionStorage",
