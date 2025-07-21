08979b36c7b646fdfbd1b4342257fb60ed56323e
Wale <walezbee@gmail.com>
2025-07-21 10:51:55 +0200

    Update authconfig.js file

--- orphaned-resource-web/src/authConfig.js
+++ orphaned-resource-web/src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "2bed0b07-e8ff-4333-9c8b-2aa020872e2d",
     authority: "https://login.microsoftonline.com/common", // Enables multi-tenant
-    redirectUri: "https://calm-cliff-00308290f.2.azurestaticapps.net.azurestaticapps.net/.auth/login/aad/callback"
+    redirectUri: "https://ambitious-moss-0f4949803.1.azurestaticapps.net"
   },
   cache: {
     cacheLocation: "sessionStorage",
