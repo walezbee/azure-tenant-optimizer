4393a66f3103a00a22594349d69e4f44759a9a73
Wale <walezbee@gmail.com>
2025-07-21 13:58:35 +0200

    Delete word from authconfig.js file

--- orphaned-resource-web/src/authConfig.js
+++ orphaned-resource-web/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
     clientId: "2bed0b07-e8ff-4333-9c8b-2aa020872e2d",
-    authority: "https://login.microsoftonline.com/common", // Enables multi-tenant access
+    authority: "https://login.microsoftonline.com/common", // Enables multi-tenant
     redirectUri: "https://ambitious-moss-0f4949803.1.azurestaticapps.net/.auth/login/aad/callback"
   },
   cache: {
