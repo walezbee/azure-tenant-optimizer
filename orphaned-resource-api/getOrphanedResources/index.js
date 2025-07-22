f081a2bba89eb7674814c5ed94c77f42c6a714e6
Wale <walezbee@gmail.com>
2025-07-22 13:30:35 +0200

    Update index.js file

--- orphaned-resource-api/getOrphanedResources/index.js
+++ orphaned-resource-api/getOrphanedResources/index.js
@@ -1,5 +1,20 @@
-const { DefaultAzureCredential } = require("@azure/identity");
 const { ResourceManagementClient } = require("@azure/arm-resources");
+const { ClientSecretCredential, TokenCredential } = require("@azure/identity");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { TokenCredentialAuthenticationProvider } = require("@azure/ms-rest-js");
+
+class AccessTokenCredential {
+  constructor(token) {
+    this.token = token;
+  }
+
+  getToken() {
+    return Promise.resolve({
+      token: this.token,
+      expiresOnTimestamp: Date.now() + 60 * 60 * 1000,
+    });
+  }
+}
 
 module.exports = async function (context, req) {
   context.log("getOrphanedResources function invoked");
@@ -18,19 +33,30 @@ module.exports = async function (context, req) {
     const accessToken = authHeader.split(" ")[1];
     context.log("Received access token");
 
-    // TEMPORARY: Respond with dummy data (remove this block when real logic is ready)
+    const credential = new AccessTokenCredential(accessToken);
+    const client = new ResourceManagementClient(credential, "<any-subscription-id>");
+
+    // Instead of hardcoding subscription, get all subscriptions the token has access to
+    const subscriptions = await client.subscriptions.list();
+
+    let allResources = [];
+
+    for await (const subscription of subscriptions) {
+      const subClient = new ResourceManagementClient(credential, subscription.subscriptionId);
+      const resources = [];
+
+      for await (const resource of subClient.resources.list()) {
+        resources.push(resource);
+      }
+
+      allResources.push(...resources);
+    }
+
+    // You can filter orphaned/deprecated resources later here
+
     context.res = {
       status: 200,
-      body: {
-        data: [
-          {
-            id: "dummy-resource-id",
-            name: "orphaned-vm",
-            type: "Microsoft.Compute/virtualMachines",
-            location: "westeurope",
-          },
-        ],
-      },
+      body: { data: allResources },
     };
   } catch (error) {
     context.log.error("Error processing request:", error.message);
